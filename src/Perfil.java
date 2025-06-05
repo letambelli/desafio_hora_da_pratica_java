f2865cd2e71bbc2b5b7c9e8b4bed05108cd0cda6
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-06-04 22:59:51 -0300

    funcao estudos

--- src/Perfil.java
+++ src/Perfil.java
@@ -1,6 +1,7 @@
-public class Main {
+public class Perfil {
     public static void main(String[] args) {
         System.out.println("Olá, Nome");
         System.out.println("Tudo bem?");
     }
 }
+
